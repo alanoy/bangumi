6a851c5d21d79d55953b31cbae827214ba5b3697
Alan <alanoy11@gmail.com>
2024-01-15 11:59:43 +0800

    refactor: utilize the object element to designate a default image in case of an error with the bgmtv image.

--- components/bgm/Cover.vue
+++ components/bgm/Cover.vue
@@ -1,15 +1,32 @@
 <script lang="ts" setup>
-defineProps<{
+const props = defineProps<{
   item: BgmItem
 }>()
+
+const image = computed(() => {
+  const target = props.item.images?.large || '/not-available.png'
+  const suffix = target.split('.').pop()
+
+  return {
+    default: '/not-available.png',
+    target,
+    type: `image/${suffix}`,
+  }
+})
 </script>
 
 <template>
   <figure class="cover bg-cover bg-top bg-no-repeat relative bg-white top-0 left-0 bottom-0">
-    <img
+    <object
       class="object-cover h-full"
-      :src="item.images?.large || '/not-available.png'"
-    />
+      :data="image.target"
+      :type="image.type"
+    >
+      <img
+        class="object-cover h-full"
+        :src="image.default"
+      />
+    </object>
     <span
       v-if="item.rating && item.rating.score > 0"
       class="absolute w-full left-0 bottom-0 h-6 text-xs text-center bg-black/[0.65] text-white py-1 whitespace-nowrap"
