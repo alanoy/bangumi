359200fb73f0ab87fdfe3e462d5f1eea75cd50c3
Alan <alanoy11@gmail.com>
2024-01-22 22:33:31 +0800

    fix: get user api

--- composables/useBgmtv.ts
+++ composables/useBgmtv.ts
@@ -2,8 +2,6 @@ import type { BgmtvAuthRequestBody, BgmtvAuthResponse, BgmtvUser } from '@/types
 
 export const useBgmtv = () => {
   const isLogin = useState(() => false)
-  const user = useState<BgmtvUser | null>(() => null)
-  const userPending = ref(false)
 
   async function authorize(
     params: BgmtvAuthRequestBody,
@@ -18,29 +16,6 @@ export const useBgmtv = () => {
     }
 
     return res
-  }
-
-  async function getUser(onSuccess: (user: BgmtvUser) => void) {
-    if (!isLogin.value || user.value) {
-      if (user.value) {
-        onSuccess && onSuccess(user.value)
-      }
-
-      return
-    }
-
-    if (userPending.value) {
-      return
-    }
-
-    userPending.value = true
-    const { data } = (await $fetch('/api/user')) as { data?: BgmtvUser }
-
-    userPending.value = false
-    if (data) {
-      user.value = data
-      onSuccess && onSuccess(data)
-    }
   }
 
   async function logout() {
@@ -61,8 +36,6 @@ export const useBgmtv = () => {
   return {
     isLogin,
     authorize,
-    getUser,
-    user,
     logout,
     setLogin,
   }
