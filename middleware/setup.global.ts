6b3c3b4afe55b14e12767b3b56fa9e8017f12a0c
Alan <alanoy11@gmail.com>
2024-01-22 22:38:32 +0800

    fix: get user api

--- middleware/setup.global.ts
+++ middleware/setup.global.ts
@@ -1,8 +1,10 @@
+import { useBgmtvFetch } from '@/composables/useBgmtvFetch'
 import type { BgmtvUser } from '@/types/bgmtv'
 
 export default defineNuxtRouteMiddleware(async () => {
   const { setLogin } = useBgmtv()
   const { getSession, updateSession } = useH3Session()
+  const { fetch } = useBgmtvFetch()
 
   if (process.server) {
     const event = useRequestEvent()
@@ -12,7 +14,8 @@ export default defineNuxtRouteMiddleware(async () => {
 
     if (data.access_token && !data.user) {
       // set user data to session
-      const { data } = (await $fetch('/api/user')) as { data?: BgmtvUser }
+      const data = (await fetch('/v0/me', {}, event)) as BgmtvUser
+
       if (data) {
         await updateSession(event, {}, { user: data })
       }
