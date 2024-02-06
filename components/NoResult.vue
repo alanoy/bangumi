92118df4cc922ed087a6cfdc8679c5c4144689c5
Alan <alanoy11@gmail.com>
2024-02-06 18:50:00 +0800

    fix: ui

--- components/NoResult.vue
+++ components/NoResult.vue
@@ -5,7 +5,7 @@ defineProps<{
 </script>
 
 <template>
-  <main class="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+  <main class="grid min-h-full grow place-items-center px-6 py-24 sm:py-32 lg:px-8">
     <div class="text-center">
       <h2 class="mt-10 text-3xl font-bold tracking-tight sm:text-5xl">
         {{ $t('no-result') }}
