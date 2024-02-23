57250d4df328c99ed45a1877cee2dc8af2f1481e
Alan <alanoy11@gmail.com>
2024-02-23 09:56:15 +0800

    fix: layout ui

--- app.vue
+++ app.vue
@@ -14,14 +14,16 @@ setMetas((data.value as { metas: BgmMeta })?.metas || {})
 </script>
 
 <template>
-  <div class="flex flex-col min-h-svh">
+  <div class="flex flex-col justify-between min-h-dvh">
     <NuxtLoadingIndicator
       class="flex-none"
       :throttle="0"
       color="repeating-linear-gradient(to right,#da5597 0%,#9d59ef 50%,#6466e9 100%)"
     />
     <LayoutHeader />
-    <NuxtPage class="md:p-5 sm:p-3 p-3 flex-auto grow" />
+    <div class="md:p-5 sm:p-3 p-3 flex-auto grow">
+      <NuxtPage />
+    </div>
     <LayoutFooter />
     <Toast />
   </div>
--- pages/about.vue
+++ pages/about.vue
@@ -8,42 +8,40 @@ useHead({
 </script>
 
 <template>
-  <section class="bg-base-100">
-    <div class="p-5 leading-10">
-      <p>
-        {{ appTitle }} App by
-        <a
-          href="//ideapart.com/"
-          title="ideapart"
-          target="_blank"
-          class="link link-secondary"
-        >
-          Alan
-        </a>
-      </p>
-      <div class="mt-4">
-        {{ $t('sources.data') }}:
-        <ul class="ml-8 list-disc list-inside">
-          <li>
-            <a
-              href="https://github.com/bangumi-data/bangumi-data.git"
-              rel="noreferrer"
-              class="link"
-            >
-              bangumi-data
-            </a>
-          </li>
-          <li>
-            <a
-              href="http://bgm.tv/"
-              rel="noreferrer"
-              class="link"
-            >
-              bgm.tv
-            </a>
-          </li>
-        </ul>
-      </div>
+  <div class="p-5 leading-10">
+    <p>
+      {{ appTitle }} App by
+      <a
+        href="//ideapart.com/"
+        title="ideapart"
+        target="_blank"
+        class="link link-secondary"
+      >
+        Alan
+      </a>
+    </p>
+    <div class="mt-4">
+      {{ $t('sources.data') }}:
+      <ul class="ml-8 list-disc list-inside">
+        <li>
+          <a
+            href="https://github.com/bangumi-data/bangumi-data.git"
+            rel="noreferrer"
+            class="link"
+          >
+            bangumi-data
+          </a>
+        </li>
+        <li>
+          <a
+            href="http://bgm.tv/"
+            rel="noreferrer"
+            class="link"
+          >
+            bgm.tv
+          </a>
+        </li>
+      </ul>
     </div>
-  </section>
+  </div>
 </template>
