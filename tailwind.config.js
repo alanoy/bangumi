1058a6877e3227291f26333289bcc5b4039d8e16
Alan <alanoy11@gmail.com>
2025-05-20 13:12:00 +0800

    refactor: add Tailwind CSS and Daisy UI themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-import { themes } from './config'
 export default {
-  plugins: [require('daisyui')],
   darkMode: 'class',
-  daisyui: {
-    themes,
-  },
 }
